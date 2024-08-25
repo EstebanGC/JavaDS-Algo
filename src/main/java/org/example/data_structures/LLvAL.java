2f803eb3867f76b161a644aec051d655df4d52d7
EstebanGC <johan.gal.car@gmail.com>
2024-08-24 20:02:09 -0500

    LinkedList vs ArrayList

---
+++ src/main/java/org/example/data_structures/LLvAL.java
@@ -0,0 +1,14 @@
+package org.example.data_structures;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LLvAL {
+
+    public void time() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+
+    }
+}
