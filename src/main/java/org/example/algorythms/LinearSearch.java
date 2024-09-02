0a962c05fc4b281c62793430c051c8eeaf7c83bd
EstebanGC <johan.gal.car@gmail.com>
2024-09-01 22:28:33 -0500

    Algorythms
    Binary search

--- src/main/java/org/example/algorythms/LinearSearch.java
+++ src/main/java/org/example/algorythms/LinearSearch.java
@@ -1,7 +1,22 @@
 package org.example.algorythms;
 
+import java.util.Arrays;
+
 public class LinearSearch {
     public void whatever() {
-        System.out.println("Whatever");
+        int array[] = new int[100];
+        int target = 42;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        int index = Arrays.binarySearch(array, target);
+
+        if(index >= 0) {
+            System.out.println(target + " not found");
+        } else {
+            System.out.println(target + " found at index " + index );
+        }
     }
 }
