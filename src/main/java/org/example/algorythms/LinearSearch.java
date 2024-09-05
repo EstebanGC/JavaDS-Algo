9c6462447d03fe616199dc3c99f88057df99df77
EstebanGC <johan.gal.car@gmail.com>
2024-09-04 22:00:50 -0500

    Binary Search donde

--- src/main/java/org/example/algorythms/LinearSearch.java
+++ src/main/java/org/example/algorythms/LinearSearch.java
@@ -24,6 +24,17 @@ public class LinearSearch {
         int low = 0;
         int high = array.length - 1;
 
-        return -1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            int value = array[middle];
+
+            System.out.println("middle: " + value);
+
+            if (value < target) low = middle + 1;
+                else if (value > target) high = middle - 1;
+                else return middle;
+            }
+
+        return -1;//target not found
     }
 }
