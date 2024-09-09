e7ca6ae2c1f4da729cf1ea253f8bfd3eda3cff9d
EstebanGC <johan.gal.car@gmail.com>
2024-09-08 22:22:22 -0500

    Linear search finished

--- src/main/java/org/example/algorythms/LinearSearch.java
+++ src/main/java/org/example/algorythms/LinearSearch.java
@@ -1,4 +1,25 @@
 package org.example.algorythms;
 
 public class LinearSearch {
+    public void findIndex(){
+        int[] array = {9,1, 2,3,4,8,6};
+
+        int index = linearSearch(array,1);
+
+        if(index != -1){
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    private static int linearSearch(int[] array, int value){
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]== value){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
