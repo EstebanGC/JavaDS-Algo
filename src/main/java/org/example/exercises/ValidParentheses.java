0334c5009ebfb931030d5224458636380d03ec57
EstebanGC <johan.gal.car@gmail.com>
2024-09-11 18:23:52 -0500

    ValidParentheses donde

--- src/main/java/org/example/exercises/ValidParentheses.java
+++ src/main/java/org/example/exercises/ValidParentheses.java
@@ -6,10 +6,18 @@ public class ValidParentheses {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(')');
-
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == ')') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '}') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == ']') {
+                stack.pop();
+            } else {
+                return false;
             }
         }
+        return stack.isEmpty();
     }
 }
